7985461d4bfe63b366b4e2ff6376b08414916bbf
Humberto Solano <moonsmileh.project@gmail.com>
2016-10-08 09:39:48 -0500

    Initial Commit

---
+++ Controles/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Controles
+//
+//  Created by Humberto Solano on 08/10/16.
+//  Copyright © 2016 moonsmileh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
